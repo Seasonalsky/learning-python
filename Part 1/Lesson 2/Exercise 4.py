e95fa5d97c3bd13709ff6ff140196c1f7f59e0b4
Luna Ong <jeweliaisme@gmail.com>
2020-01-18 16:57:16 +0800

    Update Exercise 4.py

--- Part 1/Lesson 2/Exercise 4.py
+++ Part 1/Lesson 2/Exercise 4.py
@@ -1,2 +1,9 @@
 #https://www.py4e.com/html3/02-variables
-#Exercise 4: Assume that we execute the following assignment statements
+#Exercise 5: Write a program which prompts the user for a Celsius temperature,
+#convert the temperature to Fahrenheit, and print out the converted temperature.
+print('Enter Celsius temperature:')
+c = input()
+c=float(c)
+f = (c*(9/5))+32
+f=str(f)
+print('Farenheit temperature is:' + f)
